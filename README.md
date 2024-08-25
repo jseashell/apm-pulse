c71a727a02819e8684885f950d048f8bafb5ff74
John Schellinger <schellingerj1@gmail.com>
2024-08-25 15:51:55 -0400

    Add some commands and settings, clean up code, re-introduce eslint and testing scaffolding

--- README.md
+++ README.md
@@ -1,18 +1,40 @@
 <img src="docs/logo.png" width="256" height="256" alt="Logo" />
 
-Real-time, game-style APM counter for your VS Code status bar.
+Game-style APM counter for your VS Code status bar.
 
 ![Example](docs/example.gif)
 
 ## Features
 
-- Counts actions while you code or use the terminal.
-- Displays real-time actions per minute in the bottom status bar (keyboard icon)
+- Displays real-time actions per minute in the bottom status
+  - Typing
+  - Open/close file
+  - Cursor location in file
+  - Terminal usagebar (keyboard icon)
+- [Command palette integration](#commands)
+- [Customizable settings](#settings)
 
-## Attribution
+### Commands
 
-- Mouse by sentya irma from [Noun Project](https://thenounproject.com/browse/icons/term/mouse/) (CC BY 3.0)
+The following commands are available through the VS Code command palette (Ctrl+Shift+P).
+
+| Command      | Description                                                 |
+| :----------- | :---------------------------------------------------------- |
+| Reset totals | Resets the APM counter's total stat accumulation mechanism. |
+| Show totals  | Displays a message with your APM stats.                     |
+
+### Settings
+
+The following settings are available in the user settings screen in Extensions > APM Pulse.
+
+| Setting   | Description                             |
+| :-------- | :-------------------------------------- |
+| Alignment | Horizontal alignment in the status bar. |
 
 ## License
 
 This software is distributed under the terms of the [MIT License](/LICENSE).
+
+### Attributions
+
+- Mouse icon by sentya irma from [Noun Project](https://thenounproject.com/browse/icons/term/mouse/) (CC BY 3.0)
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,52 @@
-import { ExtensionContext, StatusBarAlignment, StatusBarItem, window } from 'vscode';
+import {
+  commands,
+  ConfigurationChangeEvent,
+  ExtensionContext,
+  StatusBarAlignment,
+  StatusBarItem,
+  window,
+  workspace,
+} from 'vscode';
 
+/** the ui element */
 let statusBarItem: StatusBarItem;
+/** update {@link statusBarItem} on an interval. cleaned up on deactivations */
+let interval: NodeJS.Timeout;
+/** counts total actions over lifespan of VSC window */
+const totals = { start: 0, actions: 0 };
+/** list of timestamp millis recorded when a valid action was made */
+const rollingActionTimestamps: number[] = [];
+/** previous n millis in which to calculate actions per minute */
+const rollingWindow = 4000;
 
-let apmInterval: NodeJS.Timeout;
-const rollingWindowMillis = 4000;
-const actionTimestamps: number[] = [];
-
-/** VS Code extension entry point */
+/**
+ * VS Code extension entry point
+ */
 export function activate(context: ExtensionContext) {
-  statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-  statusBarItem.name = 'VSC APM';
-  context.subscriptions.push(statusBarItem);
+  // Record the beginning of execution
+  totals.start = Date.now();
 
-  context.subscriptions.push(window.onDidChangeActiveTextEditor(recordAction)); // typing
+  // Setup command palette
+  context.subscriptions.push(commands.registerCommand('apm-pulse.resetTotals', resetTotals));
+  context.subscriptions.push(commands.registerCommand('apm-pulse.showTotals', showTotalsInformationMessage));
+
+  // Setup extension settings
+  context.subscriptions.push(workspace.onDidChangeConfiguration((event) => handleConfigChanges(event)));
+
+  // Create the UI
+  const config = workspace.getConfiguration('apm-pulse');
+  const alignOpt = config.get('alignment');
+  statusBarItem = window.createStatusBarItem(alignOpt == 'Right' ? StatusBarAlignment.Right : StatusBarAlignment.Left);
+  statusBarItem.name = 'APM Pulse';
+
+  // Setup change events
+  context.subscriptions.push(statusBarItem);
+  context.subscriptions.push(window.onDidChangeActiveTextEditor(recordAction));
   context.subscriptions.push(window.onDidChangeVisibleTextEditors(recordAction));
   context.subscriptions.push(window.onDidChangeTextEditorSelection(recordAction));
-  // context.subscriptions.push(window.onDidChangeTextEditorVisibleRanges(recordAction));
-  // context.subscriptions.push(window.onDidChangeTextEditorOptions(recordAction));
-  // context.subscriptions.push(window.onDidChangeTextEditorViewColumn(recordAction));
-  // context.subscriptions.push(window.onDidChangeVisibleNotebookEditors(recordAction));
-  // context.subscriptions.push(window.onDidChangeActiveNotebookEditor(recordAction));
-  // context.subscriptions.push(window.onDidChangeNotebookEditorSelection(recordAction));
-  // context.subscriptions.push(window.onDidChangeNotebookEditorVisibleRanges(recordAction));
+  context.subscriptions.push(window.onDidChangeTextEditorVisibleRanges(recordAction));
+  context.subscriptions.push(window.onDidChangeTextEditorOptions(recordAction));
+  context.subscriptions.push(window.onDidChangeTextEditorViewColumn(recordAction));
   context.subscriptions.push(window.onDidChangeActiveTerminal(recordAction));
   context.subscriptions.push(window.onDidOpenTerminal(recordAction));
   context.subscriptions.push(window.onDidCloseTerminal(recordAction));
@@ -30,36 +55,92 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(window.onDidChangeActiveTerminal(recordAction));
   context.subscriptions.push(window.onDidChangeTextEditorSelection(recordAction));
 
-  apmInterval = setInterval(() => {
+  // Setup 1s interval to update the UI
+  interval = setInterval(() => {
     const val = Math.floor(apm());
-
-    // $(icon_name) is special vsc extension syntax.
-    // icons here: https://code.visualstudio.com/api/references/icons-in-labels#icon-listing
-    statusBarItem.text = '$(keyboard) ' + val;
+    statusBarItem.text = '$(keyboard) ' + val; // $(icon_name) is vsc syntax. icons here: https://code.visualstudio.com/api/references/icons-in-labels#icon-listing
   }, 1000);
 
+  // Show the UI. Do this last.
   statusBarItem.show();
 }
 
-/** @returns the user's apm */
+/**
+ * Resets counters for total stats
+ */
+function resetTotals() {
+  totals.actions = 0;
+  totals.start = Date.now();
+}
+
+/**
+ * Shows a info message containing total stats
+ */
+function showTotalsInformationMessage() {
+  () => {
+    let time: string;
+    const ms = totals.start - Date.now();
+    const hours = Math.floor(ms / 3600000);
+    const minutes = Math.floor((ms % 3600000) / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+
+    if (hours > 0) {
+      time = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
+        2,
+        '0',
+      )}`;
+    } else {
+      time = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    }
+
+    const totalApm = totals.actions / (ms / 1000 / 60);
+    window.showInformationMessage(`${totalApm} APM over ${time}`);
+  };
+}
+
+/**
+ * Records an action to {@link rollingActionTimestamps}.
+ * Removes stale actions outside the {@link rollingWindow}.
+ */
+function recordAction(): void {
+  totals.actions++;
+
+  const now = Date.now();
+  rollingActionTimestamps.push(now);
+
+  while (rollingActionTimestamps[0] < now - rollingWindow) {
+    rollingActionTimestamps.shift();
+  }
+}
+
+/**
+ * @returns the user's apm
+ */
 function apm(): number {
   const now = Date.now();
-  const recentActions = actionTimestamps.filter((ts) => ts > now - rollingWindowMillis);
+  const recentActions = rollingActionTimestamps.filter((ts) => ts > now - rollingWindow);
   const apm = (recentActions.length / 10) * 60;
   return apm;
 }
 
-/** records an action in {@link actionTimestamps} */
-function recordAction() {
-  const now = Date.now();
-  actionTimestamps.push(now);
-
-  while (actionTimestamps[0] < now - rollingWindowMillis) {
-    actionTimestamps.shift();
+/**
+ * Handles extension settings updates during runtime
+ * @param event config change event
+ */
+function handleConfigChanges(event: ConfigurationChangeEvent): void {
+  if (event.affectsConfiguration('apm-pulse.alignment')) {
+    const alignOpt = workspace.getConfiguration('apm-pulse').get('alignment');
+    if (statusBarItem) statusBarItem.dispose();
+    statusBarItem = window.createStatusBarItem(
+      alignOpt == 'Right' ? StatusBarAlignment.Right : StatusBarAlignment.Left,
+    );
+    statusBarItem.show();
   }
 }
 
-/** VS Code extension clean up */
-export function deactivate() {
-  clearInterval(apmInterval);
+/**
+ * VS Code extension clean up
+ */
+export function deactivate(): void {
+  clearInterval(interval);
 }
